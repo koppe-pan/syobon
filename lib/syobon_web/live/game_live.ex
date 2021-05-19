1b92087037220947952693e14fa39dc7f65e227d
koppe_pan <takas3141@gmail.com>
2021-05-19 22:58:43 +0900

    Add GameCharacter as circle

--- lib/syobon_web/live/game_live.ex
+++ lib/syobon_web/live/game_live.ex
@@ -1,39 +1,21 @@
 defmodule SyobonWeb.GameLive do
   use SyobonWeb, :live_view
 
+  @width 540
+  @height 540
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    {:ok, assign(socket, me: %{x: 320, y: 320}, width: @width, height: @height)}
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
-  end
-
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
+  def handle_event("move", %{"key" => key}, socket = %{assigns: %{me: me = %{x: x, y: y}}}) do
+    case key do
+      "w" -> {:noreply, assign(socket, me: %{me | y: max(0, y - 10)})}
+      "a" -> {:noreply, assign(socket, me: %{me | x: max(0, x - 10)})}
+      "s" -> {:noreply, assign(socket, me: %{me | y: min(@height - 1, y + 10)})}
+      "d" -> {:noreply, assign(socket, me: %{me | x: min(@width - 1, x + 10)})}
     end
-  end
-
-  defp search(query) do
-    if not SyobonWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
   end
 end
